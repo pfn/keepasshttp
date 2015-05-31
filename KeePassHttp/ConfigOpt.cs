2eddf7fa0a0487980c2f036ea53f82c1ed693eb9
frankhommers <frank-github@hommers.nl>
2015-05-31 21:08:07 +0200

    Allow the host to bind to to be changed.

--- KeePassHttp/ConfigOpt.cs
+++ KeePassHttp/ConfigOpt.cs
@@ -2,90 +2,90 @@
 
 namespace KeePassHttp
 {
-    public class ConfigOpt
-    {
-        readonly AceCustomConfig _config;
-        const string ReceiveCredentialNotificationKey = "KeePassHttp_ReceiveCredentialNotification";
-        const string SpecificMatchingOnlyKey = "KeePassHttp_SpecificMatchingOnly";
-        const string UnlockDatabaseRequestKey = "KeePassHttp_UnlockDatabaseRequest";
-        const string AlwaysAllowAccessKey = "KeePassHttp_AlwaysAllowAccess";
-        const string AlwaysAllowUpdatesKey = "KeePassHttp_AlwaysAllowUpdates";
-        const string SearchInAllOpenedDatabasesKey = "KeePassHttp_SearchInAllOpenedDatabases";
-        const string MatchSchemesKey = "KeePassHttp_MatchSchemes";
-        const string ReturnStringFieldsKey = "KeePassHttp_ReturnStringFields";
-        const string SortResultByUsernameKey = "KeePassHttp_SortResultByUsername";
-        const string ListenerPortKey = "KeePassHttp_ListenerPort";
-        const string ListenerHostKey = "KeePassHttp_ListenerHost";
+	public class ConfigOpt
+	{
+		readonly AceCustomConfig _config;
+		const string ReceiveCredentialNotificationKey = "KeePassHttp_ReceiveCredentialNotification";
+		const string SpecificMatchingOnlyKey = "KeePassHttp_SpecificMatchingOnly";
+		const string UnlockDatabaseRequestKey = "KeePassHttp_UnlockDatabaseRequest";
+		const string AlwaysAllowAccessKey = "KeePassHttp_AlwaysAllowAccess";
+		const string AlwaysAllowUpdatesKey = "KeePassHttp_AlwaysAllowUpdates";
+		const string SearchInAllOpenedDatabasesKey = "KeePassHttp_SearchInAllOpenedDatabases";
+		const string MatchSchemesKey = "KeePassHttp_MatchSchemes";
+		const string ReturnStringFieldsKey = "KeePassHttp_ReturnStringFields";
+		const string SortResultByUsernameKey = "KeePassHttp_SortResultByUsername";
+		const string ListenerPortKey = "KeePassHttp_ListenerPort";
+		const string ListenerHostKey = "KeePassHttp_ListenerHost";
 
-        public ConfigOpt(AceCustomConfig config)
-        {
-            _config = config;
-        }
+		public ConfigOpt(AceCustomConfig config)
+		{
+			_config = config;
+		}
 
-        public bool ReceiveCredentialNotification
-        {
-            get { return _config.GetBool(ReceiveCredentialNotificationKey, true); }
-            set { _config.SetBool(ReceiveCredentialNotificationKey, value); }
-        }
+		public bool ReceiveCredentialNotification
+		{
+			get { return _config.GetBool(ReceiveCredentialNotificationKey, true); }
+			set { _config.SetBool(ReceiveCredentialNotificationKey, value); }
+		}
 
-        public bool UnlockDatabaseRequest
-        {
-            get { return _config.GetBool(UnlockDatabaseRequestKey, false); }
-            set { _config.SetBool(UnlockDatabaseRequestKey, value); }
-        }
+		public bool UnlockDatabaseRequest
+		{
+			get { return _config.GetBool(UnlockDatabaseRequestKey, false); }
+			set { _config.SetBool(UnlockDatabaseRequestKey, value); }
+		}
 
-        public bool SpecificMatchingOnly
-        {
-            get { return _config.GetBool(SpecificMatchingOnlyKey, false); }
-            set { _config.SetBool(SpecificMatchingOnlyKey, value); }
-        }
+		public bool SpecificMatchingOnly
+		{
+			get { return _config.GetBool(SpecificMatchingOnlyKey, false); }
+			set { _config.SetBool(SpecificMatchingOnlyKey, value); }
+		}
 
-        public bool AlwaysAllowAccess
-        {
-            get { return _config.GetBool(AlwaysAllowAccessKey, false); }
-            set { _config.SetBool(AlwaysAllowAccessKey, value); }
-        }
+		public bool AlwaysAllowAccess
+		{
+			get { return _config.GetBool(AlwaysAllowAccessKey, false); }
+			set { _config.SetBool(AlwaysAllowAccessKey, value); }
+		}
 
-        public bool AlwaysAllowUpdates
-        {
-            get { return _config.GetBool(AlwaysAllowUpdatesKey, false); }
-            set { _config.SetBool(AlwaysAllowUpdatesKey, value); }
-        }
+		public bool AlwaysAllowUpdates
+		{
+			get { return _config.GetBool(AlwaysAllowUpdatesKey, false); }
+			set { _config.SetBool(AlwaysAllowUpdatesKey, value); }
+		}
 
-        public bool SearchInAllOpenedDatabases
-        {
-            get { return _config.GetBool(SearchInAllOpenedDatabasesKey, false); }
-            set { _config.SetBool(SearchInAllOpenedDatabasesKey, value); }
-        }
+		public bool SearchInAllOpenedDatabases
+		{
+			get { return _config.GetBool(SearchInAllOpenedDatabasesKey, false); }
+			set { _config.SetBool(SearchInAllOpenedDatabasesKey, value); }
+		}
 
-        public bool MatchSchemes
-        {
-            get { return _config.GetBool(MatchSchemesKey, false); }
-            set { _config.SetBool(MatchSchemesKey, value); }
-        }
+		public bool MatchSchemes
+		{
+			get { return _config.GetBool(MatchSchemesKey, false); }
+			set { _config.SetBool(MatchSchemesKey, value); }
+		}
 
-        public bool ReturnStringFields
-        {
-            get { return _config.GetBool(ReturnStringFieldsKey, false); }
-            set { _config.SetBool(ReturnStringFieldsKey, value); }
-        }
+		public bool ReturnStringFields
+		{
+			get { return _config.GetBool(ReturnStringFieldsKey, false); }
+			set { _config.SetBool(ReturnStringFieldsKey, value); }
+		}
 
-        public bool SortResultByUsername
-        {
-            get { return _config.GetBool(SortResultByUsernameKey, true); }
-            set { _config.SetBool(SortResultByUsernameKey, value); }
-        }
+		public bool SortResultByUsername
+		{
+			get { return _config.GetBool(SortResultByUsernameKey, true); }
+			set { _config.SetBool(SortResultByUsernameKey, value); }
+		}
 
-        public long ListenerPort
-        {
-            get { return _config.GetLong(ListenerPortKey, KeePassHttpExt.DEFAULT_PORT); }
-            set { _config.SetLong(ListenerPortKey, value); }
-        }
+		public long ListenerPort
+		{
+			get { return _config.GetLong(ListenerPortKey, KeePassHttpExt.DEFAULT_PORT); }
+			set { _config.SetLong(ListenerPortKey, value); }
+		}
 
-        public string ListenerHost
-        {
-            get { return _config.GetString(ListenerHostKey, KeePassHttpExt.DEFAULT_HOST); }
-            set { _config.SetString(ListenerHostKey, value); }
-        }
-    }
+		public string ListenerHost
+		{
+			get { return _config.GetString(ListenerHostKey, KeePassHttpExt.DEFAULT_HOST); }
+			set { _config.SetString(ListenerHostKey, value); }
+		}
+	}
 }
